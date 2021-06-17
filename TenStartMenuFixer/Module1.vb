4ccc6a70a2a2785d4d7d56803d6f186a24216e92
Konnor88 <39319697+Konnor88@users.noreply.github.com>
2021-06-17 00:41:02 -0400

    Add files via upload

---
+++ TenStartMenuFixer/Module1.vb
@@ -0,0 +1,46 @@
+﻿Imports Microsoft.Win32
+
+Module Module1
+    Sub Main()
+        Console.Title = "10StartMenu Fixer for Windows 11 by Konnor88"
+        Console.WriteLine("Welcome to the 10StartMenu Fixer for Windows 11! What would you like to do?")
+        Console.WriteLine("[1] Use Windows 10 Start menu")
+        Console.WriteLine("[2] Remove Windows 10 Start menu")
+Line1:
+        Dim knownKeyPressed As Boolean = False
+        Dim keyReaded As ConsoleKeyInfo = Console.ReadKey
+        Select Case (keyReaded.Key)
+            Case ConsoleKey.D1
+                'Number 1 Key
+
+                knownKeyPressed = True
+                Call Main2()
+            Case ConsoleKey.D2
+                'Number 2 Key
+                knownKeyPressed = True
+                Call Main3()
+            Case Else
+                Console.WriteLine("Wrong key, please try again.")
+                GoTo Line1
+                knownKeyPressed = False
+        End Select
+    End Sub
+    Sub Main2()
+        Console.WriteLine("")
+        Console.WriteLine("Applying fix...")
+        My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\", True).SetValue("Start_ShowClassicMode", 1, RegistryValueKind.DWord)
+        Console.WriteLine("Fix successful! Please restart your PC.")
+        Console.ReadLine()
+        End
+    End Sub
+    Sub Main3()
+        Console.WriteLine("")
+        Console.WriteLine("Removing fix...")
+        Dim key As Microsoft.Win32.RegistryKey
+        key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", True)
+        key.DeleteValue("Start_ShowClassicMode")
+        Console.WriteLine("Fix removed! Restart your PC to see all the changes.")
+        Console.ReadLine()
+        End
+    End Sub
+End Module
---
+++ TenStartMenuFixer/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TenStartMenuFixer")>
+<Assembly: AssemblyDescription("Restores the Windows 10 start menu on Windows 11. Can be toggled on or off.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TenStartMenuFixer")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3477676e-38e5-43e3-b51d-11733a4c10d5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
